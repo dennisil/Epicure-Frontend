53b5f813edc641d821a71a1447098b1afb5f9e81
dennisil <103424229+dennisil@users.noreply.github.com>
2022-06-07 16:03:15 +0300

    new buiddd

--- src/services/chefService.ts
+++ src/services/chefService.ts
@@ -6,7 +6,7 @@ export const chefService = {
 
 };
 
-const API_URL = "http://127.0.0.1:2718/api/v1/chef";
+const API_URL = "http://ec2-35-173-248-3.compute-1.amazonaws.com/api/v1/chef";
 
 async function query() {
   try {
--- src/services/dishService.ts
+++ src/services/dishService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const dishService = {
   query,
 };
-const API_URL = "http://127.0.0.1:2718/api/v1/dish";
+const API_URL = "http://ec2-35-173-248-3.compute-1.amazonaws.com/api/v1/dish";
 
 async function query() {
   try {
